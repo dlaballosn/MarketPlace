2e11e2160cc9c2ce81bcd646fa70faecda42652f
Nacho Gentile <nachogentile@Nachos-MacBook-Pro.local>
2013-11-23 19:36:02 +0100

    fotos

--- Backend/marketplace/markets/urls.py
+++ Backend/marketplace/markets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from views import UserView
+from views import UserView, ProductView
 
 urlpatterns = patterns('',
     url(r'user', UserView.as_view()),
+    url(r'product/list/', ProductView.as_view())
 )
